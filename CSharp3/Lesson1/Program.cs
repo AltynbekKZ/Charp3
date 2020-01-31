0ada8fe5f021519eef2fd967d7a3dd96fa022ddd
Altynbek <AltynbekKZ@users.noreply.github.com>
2020-01-31 15:26:09 +0500

    Console project

---
+++ CSharp3/Lesson1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Формирование письма
+            MailMessage mm = new MailMessage("dev.kz@yandex.kz", "altynbek040492@gmail.com");
+            mm.Subject = "Заголовок письма";
+            mm.Body = "Содержимое письма";
+            mm.IsBodyHtml = false; // Не используем html в теле письма
+
+
+            // Авторизуемся на smtp-сервере и отправляем письмо
+            SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25);
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = new NetworkCredential("dev.kz@yandex.kz", "Qwerty");
+            sc.Send(mm);
+        }
+    }
+}
